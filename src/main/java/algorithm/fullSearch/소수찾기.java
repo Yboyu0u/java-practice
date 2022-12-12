46aa7bed03296bceae4d11233af0925ac7c30680
youngwooyoo <fbduddn97@naver.com>
2022-12-12 13:15:59 +0900

    feat: [algorithm] 소수찾기

---
+++ src/main/java/algorithm/fullSearch/소수찾기.java
@@ -0,0 +1,53 @@
+package algorithm.fullSearch;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class 소수찾기 {
+
+    private static boolean[] check;
+    private static Set<Integer> set = new HashSet<>();
+    private static int count = 0;
+
+    public int solution(String numbers) {
+        // numbers: 각 종이 조각에 적힌 숫자가 적힌 문자열
+        // 종이 조각으로 만들 수 있는 소수가 몇 개인지 return
+        check = new boolean[numbers.length()];
+        search("", numbers);
+        return count;
+    }
+
+    private void search(String s, String numbers) {
+        if (!s.equals("")) {
+            int n = Integer.parseInt(s);
+            if (!set.contains(n) && isPrime(n)) {
+                set.add(n);
+                count++;
+            }
+        }
+        if (s.length() == numbers.length()) {
+            return;
+        }
+        for (int i = 0; i < numbers.length(); i++) {
+            if (!check[i]) {
+                check[i] = true;
+                search(s + numbers.charAt(i), numbers);
+                check[i] = false;
+            }
+        }
+    }
+
+    private boolean isPrime(int number) {
+        if (number == 1 || number == 0) {
+            return false;
+        }
+        int n = 2;
+        while (n < number) {
+            if (number % n == 0) {
+                return false;
+            }
+            n++;
+        }
+        return true;
+    }
+}
