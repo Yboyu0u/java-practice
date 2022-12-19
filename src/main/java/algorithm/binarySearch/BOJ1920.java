f18b0c0dd0242f0e6e6f857fc47b60bb10c5a27f
youngwooyoo <fbduddn97@naver.com>
2022-12-19 16:55:01 +0900

    feat: [algorithm] BOJ1920

---
+++ src/main/java/algorithm/binarySearch/BOJ1920.java
@@ -0,0 +1,35 @@
+package algorithm.binarySearch;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class BOJ1920 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        StringTokenizer st = new StringTokenizer(input);
+
+        int n = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            int number = Integer.parseInt(st.nextToken());
+            if (Arrays.binarySearch(arr, number) >= 0) {
+                System.out.println(1);
+                continue;
+            }
+            System.out.println(0);
+        }
+    }
+}
